ed3510b9736fb6b99bfc096c20620c9c1a5b8386
ironicsushi <yuli5565@colorado.edu>
2017-10-04 12:14:01 -0500

    * make directory and file method in FileHandler

--- src/util/FileHandler.java
+++ src/util/FileHandler.java
@@ -1,19 +1,36 @@
 package util;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 
-public class FileHandler {
+public class FileHandler{
     private String directory, fileName;
+    Path path = Paths.get("Contacts", "contacts.txt");
+
+    public static void main (String[] args) throws IOException {
+        FileHandler filehandler = new FileHandler();
+        filehandler.makeFile();
+    }
 
 //    method to create a file if it does not exist
-        public void makeFile(){
-
-        }
 //    method to create a directory if it does not exist
+        public void makeFile()throws IOException{
+            if (!Files.exists(path.getParent())) {
+                Files.createDirectory(path.getParent());
+            } if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+        }
 
 //    method for retrieving file contents as a List of Strings
 
 //    Create a method to write(append) new contents to a file. Optional parameter choose to append or overwrite
+        public void writeFile(ArrayList list) throws IOException {
+            Files.write(path, list, StandardOpenOption.APPEND);
+        }
 
 }
